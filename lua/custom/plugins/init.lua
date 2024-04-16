b94fdfb654779a37452c13814acb696011c83d2f
iuliangghinia <79693619+iuliangghinia@users.noreply.github.com>
2024-04-16 22:04:38 +0300

    Added keymaps ToggleTerm for fload

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+require 'options'
+require 'keymaps'
+require 'lazy-config'
+require 'alpha-config'
+require 'bufferline-config'
+require 'hop-config'
+require 'nvim-tree-config'
+require 'lualine-config'
+require 'lsp-config'
+require 'telescope-config'
+require 'toggleterm-config'
+require 'treesitter-config'
+require 'whichkey'
 return {}
